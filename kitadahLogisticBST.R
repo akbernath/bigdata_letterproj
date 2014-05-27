36bff3828b165055d80ebc43f67330e836c9b07b
kitadah <heather.kitada@stat.oregonstate.edu>
2014-05-27 09:33:40 -0700

    Test run from 5/26/2014

--- kitadahLogisticBST.R
+++ kitadahLogisticBST.R
@@ -101,3 +101,89 @@ while(end >=0){
 choice==true
 
 ####Generalize code to run multiple tests 
+valid<-read.csv("/Users/heatherhisako1/Documents/bigdata_letterproj/Data/xvalid_set.csv",head=TRUE)
+head(valid)
+dim(valid)
+
+specification<-c()
+for(h in 1:6667){
+  new.data<-valid[h,2:17]
+  true<-learn[h,18]
+  
+  ##RUN CODE
+  path<-c()
+  end<-1
+  
+  while(end >=0){
+    left<-c()
+    right<-c()
+    for(i in 1:26){
+      
+      ##splits data into subsets for left and right 
+      count.left<-0
+      path.left<-c(path,0)
+      for(a in 1:length(path.left)){
+        b=a+1
+        if(learn.mat[i,b]==path.left[a]){
+          count.left=count.left+1
+        }
+      }
+      if(count.left==length(path.left)){
+        left<-c(left,rownames(learn.mat)[i])
+      }
+      count.right<-0
+      path.right<-c(path,1)
+      for(a in 1:length(path.right)){
+        b=a+1
+        if(learn.mat[i,b]==path.right[a]){
+          count.right=count.right+1
+        }
+      }
+      if(count.right==length(path.right)){
+        right<-c(right,rownames(learn.mat)[i])
+      }
+    }
+    ##build a logistic regression model 
+    resp<-c(rep(NA,13333))
+    for(i in 1:13333){
+      if(learn$letter[i] %in% right){
+        resp[i]<-1
+      }
+      if(learn$letter[i] %in% left){
+        resp[i]<-0
+      }
+      
+    }
+    mod<-glm(resp~learn$x_box+learn$y_box+learn$width+learn$high+learn$onpix+learn$x_bar+learn$y_bar+learn$x2_bar+learn$y2_bar+learn$xy_bar+learn$x2y_bar+learn$xy2_bar+learn$x_ege+learn$x_egvy+learn$y_ege+learn$y_egvx,family="binomial")
+    ##this returns the sample probability from the new data 
+    this.logit<-as.numeric(mod$coef[1])
+    for(i in 2:17){
+      j=i-1
+      this.logit=this.logit+as.numeric(mod$coef[i])*as.numeric(new.data[j])
+    }
+    pi.hat=exp(this.logit)/(1+exp(this.logit))
+    ##decide where to move (right=1, left=0)
+    if(pi.hat>=0.5){
+      move=1
+    }
+    if(pi.hat<0.5){
+      move=0
+    }
+    path<-c(path,move)
+    if(move==1 & length(right)==1) {
+      choice<-right
+    }
+    if(move==0 & length(left)==1) {
+      choice<-left
+    }
+    
+    if((move==1 & length(right)==1) | (move==0 & length(left)==1)){
+      end=-1
+    }
+  }
+  
+  specification<-c(specification,choice==true)
+  print(h)
+}
+mean(specification)
+##test run on 5/26/14 --> 0.004499775
